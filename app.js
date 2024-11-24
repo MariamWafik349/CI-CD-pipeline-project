da2d50a0b3bc90429ef565526ecb8f7379e4d102
MariamWafik349 <mariamwafik10@gmail.com>
2024-11-24 20:12:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ app.get('/', (req, res) => {
   res.send('WELLCOME TO NTI');
 });
 
-const PORT = 3000; // Ensure this is capitalized and consistent
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`); // Correct variable name
+  console.log(Server running on http://localhost:${port});
 });
-
